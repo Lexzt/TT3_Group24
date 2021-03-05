8cb685d0b883f1e4022d38ee514e9f9d364f914d
valerieksy <80019203+valerieksy@users.noreply.github.com>
2021-03-05 13:38:55 +0800

    Added currentPrice

--- src/views/reports/DashboardView/TotalProfit.js
+++ src/views/reports/DashboardView/TotalProfit.js
@@ -1,15 +1,21 @@
-import React from 'react';
 import clsx from 'clsx';
 import PropTypes from 'prop-types';
+import React, { useEffect, useState } from 'react';
+import * as moment from 'moment';
+
 import {
   Avatar,
+  // Box,
   Card,
   CardContent,
   Grid,
   Typography,
-  makeStyles,
-  colors
+  colors,
+  makeStyles
 } from '@material-ui/core';
+// import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
+// import PeopleIcon from '@material-ui/icons/PeopleOutlined';
+import axios from 'axios';
 import AttachMoneyIcon from '@material-ui/icons/AttachMoney';
 
 const useStyles = makeStyles(() => ({
@@ -25,6 +31,31 @@ const useStyles = makeStyles(() => ({
 
 const TotalProfit = ({ className, ...rest }) => {
   const classes = useStyles();
+
+  const [currentPrice, setCurrentPrice] = useState(0);
+  useEffect(() => {
+    const fetchData = async () => {
+      const temp = JSON.stringify({
+        accountKey: 'd075764b-c0de-4c6d-9794-f2de8389fa43'
+      });
+
+      console.log(fetchData);
+      const config = {
+        method: 'post',
+        url: 'https://849rs099m3.execute-api.ap-southeast-1.amazonaws.com/techtrek/pricing/current',
+        headers: {
+          'x-api-key': '7hurCytKCQx5oqxCHwgx7K7jkNtBp4A71JmesZ0e',
+          'Content-Type': 'application/json'
+        },
+        data: temp
+      };
+
+      const result = await axios(config);
+      console.log(result.data);
+      setCurrentPrice(result.data);
+    };
+    fetchData();
+  }, []);
 
   return (
     <Card
@@ -43,13 +74,18 @@ const TotalProfit = ({ className, ...rest }) => {
               gutterBottom
               variant="h6"
             >
-              TOTAL PROFIT
+              CURRENT PRICE
             </Typography>
             <Typography
               color="textPrimary"
               variant="h3"
             >
-              $23,200
+              {`${currentPrice.assetSymbol} ${currentPrice.price}`}
+            </Typography>
+            <Typography color="textSecondary" variant="caption">
+              {moment
+                .unix(currentPrice.timestamp)
+                .format('dddd, MMMM Do, YYYY h:mm:ss A')}
             </Typography>
           </Grid>
           <Grid item>
